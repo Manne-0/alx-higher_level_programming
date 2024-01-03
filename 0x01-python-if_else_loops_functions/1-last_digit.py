5553bb05958c2bb1983b165f378a9809e8dc13d5
Mannie <okoromannie@gmail.com>
2024-01-03 09:56:06 -0800

    if else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if int(str(number)[-1]) > 5:
-    print('Last digit of ' + str(number) + ' is ' + str(number)[-1] + ' and is greater than 5')
-elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
-    print('Last digit of ' + str(number) + ' is ' + str(number)[-1] + ' and is less than 6 and not 0')
+if number < 0:
+    negativeNum = int(str(number)[-1]) * -1
+    print('Last digit of ' + str(number) + ' is ' + str(negativeNum) +
+        ' and is less than 6 and not 0')
 else:
-    print('Last digit of ' + str(number) + ' is ' + str(number)[-1] + 'and is 0')
+    if int(str(number)[-1]) > 5:
+        print('Last digit of ' + str(number) + ' is ' + str(number)[-1] +
+        ' and is greater than 5')
+    elif int(str(number)[-1]) < 6 and int(str(number)[-1]) != 0:
+        print('Last digit of ' + str(number) + ' is ' + str(number)[-1] +
+        ' and is less than 6 and not 0')
+    else:
+        print('Last digit of ' + str(number) + ' is ' +
+        str(number)[-1] + ' and is 0')
