6d5dd0fbbaf3ee821fae2c850339f5b914f839b3
Mannie <okoromannie@gmail.com>
2024-01-07 13:33:37 -0800

    print functions in a file

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    funtionName = dir()
+    for name in range(0, len(functionName)):
+        if funtionName[name][:2] != "_":
+            print("{:s}".format(functionName[name]))
